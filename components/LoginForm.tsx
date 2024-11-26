e5a5d849930d9b4d3720dd9db7ff21362b2cac1c
Yannik Wissner <y.wissner@reply.de>
2024-11-26 18:41:25 +0100

    feat: refactor LoginForm and RegisterForm to use react-hook-form with Zod validation

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,88 +1,79 @@
 "use client";
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { FormSubmitButton } from "@/components/forms/elements/form-submit-button";
 import { createClient } from "@/lib/supabase/client";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { FormEventHandler, useState } from "react";
+import { FormSubmitButton } from "@/components/forms/elements/form-submit-button";
+import { FormInputField } from "@/components/forms/elements/form-input-field";
+import { Form } from "@/components/ui/form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+type LoginFormValues = z.infer<typeof loginSchema>;
 
 export function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const form = useForm<LoginFormValues>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-  const supabase = createClient();
   const router = useRouter();
+  const supabase = createClient();
 
-  const handleLogin: FormEventHandler<HTMLFormElement> = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
+  const onSubmit = async (data: LoginFormValues) => {
+    const { email, password } = data;
     const { error } = await supabase.auth.signInWithPassword({
       email,
       password,
     });
-
     if (error) {
-      setError(error.message);
+      form.setError("root", { message: error.message });
     } else {
       router.push("/rooms");
     }
-    setIsLoading(false);
   };
 
   return (
-    <form
-      className="bg-white shadow-md rounded-lg p-8 w-full max-w-md"
-      onSubmit={handleLogin}
-    >
-      <div className="mb-4">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="email"
-        >
-          Email
-        </label>
-        <Input
-          id="email"
-          type="email"
+    <div className="bg-white shadow-md rounded-lg p-8 w-full max-w-md">
+      <Form {...form} onSubmit={form.handleSubmit(onSubmit)}>
+        <FormInputField<LoginFormValues>
+          name="email"
+          control={form.control}
           placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          autoComplete="username"
+          label="Email"
+          description="Enter your email address."
         />
-      </div>
-      <div className="mb-6">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="password"
-        >
-          Password
-        </label>
-        <Input
-          id="password"
+        <FormInputField<LoginFormValues>
+          name="password"
+          control={form.control}
           type="password"
           placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          autoComplete="current-password"
+          label="Password"
+          description="Enter your password."
         />
-      </div>
-      {error && <p className="text-red-500 text-xs italic mb-4">{error}</p>}
-      <div className="flex items-center justify-between">
-        <FormSubmitButton
-          variant="default"
-          size="lg"
-          type="submit"
-          isLoading={isLoading}
-        >
-          Login
-        </FormSubmitButton>
-        <Button variant="link" size="sm" asChild>
-          <Link href="/register">Register</Link>
-        </Button>
-      </div>
-    </form>
+        {form.formState.errors.root && (
+          <p className="text-red-500 text-xs italic mb-4">
+            {form.formState.errors.root.message}
+          </p>
+        )}
+        <div className="flex items-center justify-between">
+          <FormSubmitButton type="submit" className="place-self-center">
+            Login
+          </FormSubmitButton>
+          <Button variant="outline" asChild>
+            <Link href="/register">Register instead</Link>
+          </Button>
+        </div>
+      </Form>
+    </div>
   );
 }
--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -1,87 +1,76 @@
 "use client";
-import { FormEventHandler, useState } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { createClient } from "@/lib/supabase/client";
 import Link from "next/link";
-import { Input } from "@/components/ui/input";
 import { FormSubmitButton } from "@/components/forms/elements/form-submit-button";
+import { FormInputField } from "@/components/forms/elements/form-input-field";
+import { Form } from "@/components/ui/form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const registerSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+type RegisterFormValues = z.infer<typeof registerSchema>;
 
 export function RegisterForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter();
+  const form = useForm<RegisterFormValues>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
+  const router = useRouter();
   const supabase = createClient();
 
-  const handleRegister: FormEventHandler<HTMLFormElement> = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-    });
+  const onSubmit = async (data: RegisterFormValues) => {
+    const { email, password } = data;
+    const { error } = await supabase.auth.signUp({ email, password });
     if (error) {
-      setError(error.message);
+      form.setError("root", { message: error.message });
     } else {
       router.push("/rooms");
     }
-    setIsLoading(false);
   };
 
   return (
-    <form
-      className="bg-white shadow-md rounded-lg p-8 w-full max-w-md"
-      onSubmit={handleRegister}
-    >
-      <div className="mb-4">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="email"
-        >
-          Email
-        </label>
-        <Input
-          id="email"
-          type="email"
+    <div className="bg-white shadow-md rounded-lg p-8 w-full max-w-md">
+      <Form {...form} onSubmit={form.handleSubmit(onSubmit)}>
+        <FormInputField<RegisterFormValues>
+          name="email"
+          control={form.control}
           placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          autoComplete="username"
+          label="Email"
+          description="Enter your email address."
         />
-      </div>
-      <div className="mb-6">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="password"
-        >
-          Password
-        </label>
-        <Input
-          id="password"
+        <FormInputField<RegisterFormValues>
+          name="password"
+          control={form.control}
           type="password"
           placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          autoComplete="new-password"
+          label="Password"
+          description="Enter a password with at least 8 characters."
         />
-      </div>
-      {error && <p className="text-red-500 text-xs italic mb-4">{error}</p>}
-      <div className="flex items-center justify-between">
-        <FormSubmitButton
-          variant="default"
-          size="lg"
-          type="submit"
-          isLoading={isLoading}
-        >
-          Register
-        </FormSubmitButton>
-        <Button variant="link" size="sm" asChild>
-          <Link href="/login">Login</Link>
-        </Button>
-      </div>
-    </form>
+        {form.formState.errors.root && (
+          <p className="text-red-500 text-xs italic mb-4">
+            {form.formState.errors.root.message}
+          </p>
+        )}
+        <div className="flex items-center justify-between">
+          <FormSubmitButton type="submit" className="place-self-center">
+            Register
+          </FormSubmitButton>
+          <Button variant="outline" asChild>
+            <Link href="/login">Login instead</Link>
+          </Button>
+        </div>
+      </Form>
+    </div>
   );
 }
