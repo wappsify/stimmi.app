cbfdf40cc0e2e635a3905d4b5ae28f0b97c72de9
Yannik Wissner <y.wissner@reply.de>
2024-11-26 15:51:05 +0100

    feat: add NextTopLoader for improved page loading experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Noto_Sans } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import NextTopLoader from "nextjs-toploader";
 
 const noto = Noto_Sans({ subsets: ["latin"], display: "swap" });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${noto.className} antialiased bg-gray-100`}>
+        <NextTopLoader color="#0f172a" showSpinner={false} />
         {children}
         <Toaster position="bottom-center" />
       </body>
