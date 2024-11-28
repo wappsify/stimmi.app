91d1b87531ec44388daf5f9adbfbe95b19bd5594
Yannik Wissner <y.wissner@reply.de>
2024-11-28 07:26:52 +0100

    feat: enhance VotingSection with user joining logic and real-time updates; add RoomUser type definition

--- components/voting-section.tsx
+++ components/voting-section.tsx
@@ -1,4 +1,6 @@
-import { DoorOpen } from "lucide-react";
+"use client";
+
+import { DoorOpen, Loader2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -9,10 +11,57 @@ import {
 } from "@/components/ui/card";
 import { Room } from "../room.types";
 import { Choice } from "../choice.types";
+import { useEffect, useState } from "react";
+import { createClient } from "@/lib/supabase/client";
+import { useRealtimeRoomUsers } from "../lib/hooks/useRealtimeRoomUsers";
+import { RoomUser } from "../room_user.types";
+import { useUser } from "../lib/hooks/useUser";
+import { Skeleton } from "./ui/skeleton";
+import { VotingForm } from "./voting-form";
 
-export const VotingSection: React.FC<{ room: Room; choices: Choice[] }> = ({
-  room,
-}) => {
+export const VotingSection: React.FC<{
+  room: Room;
+  choices: Choice[];
+  roomUsers: RoomUser[];
+}> = ({ room, choices, roomUsers: serverRoomUsers }) => {
+  const [isJoining, setIsJoining] = useState(false);
+  const [isInRoom, setIsInRoom] = useState(false);
+
+  const user = useUser();
+
+  const supabase = createClient();
+
+  const handleJoinClick = async () => {
+    if (!user) {
+      console.error("User not found");
+      return;
+    }
+
+    setIsJoining(true);
+
+    const { error: roomUsersError } = await supabase
+      .from("room_users")
+      .insert([{ room_id: room.id, user_id: user.id, has_voted: false }]);
+
+    if (roomUsersError) {
+      console.error("Error adding room user", roomUsersError);
+      setIsJoining(false);
+      return;
+    }
+
+    setIsJoining(false);
+  };
+
+  const roomUsers = useRealtimeRoomUsers(room.id, serverRoomUsers);
+
+  useEffect(() => {
+    if (!user || !roomUsers) {
+      return;
+    }
+    const userInRoom = roomUsers.find((ru) => ru.user_id === user.id);
+    setIsInRoom(!!userInRoom);
+  }, [user, room, roomUsers]);
+
   return (
     <Card className="h-full">
       <CardHeader className="text-center">
@@ -22,10 +71,20 @@ export const VotingSection: React.FC<{ room: Room; choices: Choice[] }> = ({
         )}
       </CardHeader>
       <CardContent className="grid">
-        <Button className="place-self-center">
-          <DoorOpen />
-          Join this room and start voting!
-        </Button>
+        {!user ? (
+          <Skeleton className="h-[40px] w-full" />
+        ) : isInRoom ? (
+          <VotingForm choices={choices} />
+        ) : (
+          <Button
+            className="place-self-center"
+            onClick={handleJoinClick}
+            disabled={isJoining}
+          >
+            {isJoining ? <Loader2 /> : <DoorOpen />}
+            Join this room and start voting!
+          </Button>
+        )}
       </CardContent>
     </Card>
   );
---
+++ room_user.types.ts
@@ -0,0 +1,3 @@
+import { Database } from "./database.types";
+
+export type RoomUser = Database["public"]["Tables"]["room_users"]["Row"];
