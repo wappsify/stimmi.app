d7e03e8555fd63dca69eab17408c6b27c53e44a0
Yannik Wissner <y.wissner@reply.de>
2024-12-19 13:25:44 +0100

    feat: implement basic automatic language detection

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,8 +1,26 @@
 import { getRequestConfig } from "next-intl/server";
+import { createClient } from "../lib/supabase/server";
+import { cookies, headers } from "next/headers";
+import { resolveAcceptLanguage } from "resolve-accept-language";
+
+const FALLBACK_LOCALE = "de-DE";
+const AVAILABLE_LOCALES = ["en-US", "de-DE"];
 
 export default getRequestConfig(async () => {
-  // TODO: fetch locale from user / browser settings
-  const locale = "de";
+  const supabase = createClient(cookies());
+  const user = await supabase.auth.getUser();
+  const acceptLanguageHeader = headers().get("Accept-Language");
+  console.log("acl", acceptLanguageHeader);
+
+  const locale =
+    user?.data.user?.user_metadata.locale ??
+    (acceptLanguageHeader
+      ? resolveAcceptLanguage(
+          acceptLanguageHeader,
+          AVAILABLE_LOCALES,
+          FALLBACK_LOCALE
+        )
+      : FALLBACK_LOCALE);
 
   return {
     locale,
