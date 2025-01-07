7f3cba5bf748c1cd84b3da9ae1fa2f82902f3a89
Yannik Wissner <y.wissner@reply.de>
2025-01-07 12:12:26 +0100

    refactor: enhance ESLint configuration with type checking and rule overrides

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,31 +2,41 @@ import pluginNext from "@next/eslint-plugin-next";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+const optionsForTypeChecked = {
+  languageOptions: {
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+};
+
+const nextPlugin = {
+  plugins: {
+    "@next/next": pluginNext,
+  },
+  files: ["**/*.ts", "**/*.tsx"],
+  rules: {
+    ...pluginNext.configs["core-web-vitals"].rules,
+  },
+};
+
+const ignores = { ignores: ["node_modules", ".next", "*.config.mjs"] };
+
+const ruleOverrides = {
+  rules: {
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+  },
+};
+
 const config = tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
   tseslint.configs.stylisticTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    plugins: {
-      "@next/next": pluginNext,
-    },
-  },
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    rules: {
-      ...pluginNext.configs.recommended.rules,
-      ...pluginNext.configs["core-web-vitals"].rules,
-    },
-  },
-  { ignores: ["node_modules", ".next", "eslint.config.mjs"] }
+  optionsForTypeChecked,
+  nextPlugin,
+  ruleOverrides,
+  ignores
 );
 
 export default config;
