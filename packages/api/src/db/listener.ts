35195f1e969ebd55829529eada9711cd46b316a2
Yannik Wissner <hello@yannik.im>
2025-02-26 15:55:45 +0100

    fix: fix docker setup

--- packages/api/src/db/listener.ts
+++ packages/api/src/db/listener.ts
@@ -3,16 +3,20 @@ import { Client } from "pg";
 import { handleTableEvents } from "../sockets/handleTableEvents";
 
 export const setupChangesListener = () => {
-  const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-  });
-  void client.connect();
+  try {
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL,
+    });
+    void client.connect();
 
-  client.on("notification", handleTableEvents);
+    client.on("notification", handleTableEvents);
 
-  void client.query("LISTEN table_changes");
+    void client.query("LISTEN table_changes");
 
-  return client;
+    return client;
+  } catch (error) {
+    console.error("Error setting up changes listener", error);
+  }
 };
 
 export const destroyChangesListener = async (client: Client) => {
